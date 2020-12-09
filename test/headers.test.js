3b807b6f041bfe1e64c9a9ea2ede03331b82f1a5
rizwan199811 <rizwan.199811@gmail.com>
2020-12-09 19:41:04 +0500

    slight changes

--- test/headers.test.js
+++ test/headers.test.js
@@ -1,12 +1,23 @@
 const puppeteer =require('puppeteer');
-test('Add two numbers',()=>{    
-const sum =1+2;
-expect(sum).toEqual(3);
-})
-
-test('We can launch a browser',async ()=>{
-    const browser =await puppeteer.launch({
+let browser,newPage;
+beforeEach(async()=>{
+     browser =await puppeteer.launch({
         headless:false
     })
-    const newPage = await browser.newPage();
+    newPage = await browser.newPage();
+    await newPage.goto('localhost:3000')
+})
+
+afterEach(async()=>{
+    await browser.close();
+})
+test('We can launch a browser',async ()=>{
+    const text =await newPage.$eval('a.brand-logo',el=>el.innerHTML)
+    expect(text).toEqual('Blogster');
+})
+
+test('clicking login starts oauth flow',async ()=>{
+    await newPage.click('.right a');
+    const url =await newPage.url();
+    console.log(url);
 })
